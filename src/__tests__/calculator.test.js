173abf7ecc36752d0251f1548692435cd543e553
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-01 14:35:46 -0300

    feat: create subtraction test

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -4,3 +4,7 @@ import { add } from '../components/calculator';
 test('Sums two addends correctly', () => {
   expect(add(1 + 2)).toBe(3);
 });
+
+test('Subtracts two numbers correctly', () => {
+  expect(multiply(4 - 5)).toBe(-1);
+});
