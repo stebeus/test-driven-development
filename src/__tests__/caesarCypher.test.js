ee5434e80204855e9b6a3afc04a1de4c990f8584
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-01 16:42:48 -0300

    feat: create three letter shift test

--- src/__tests__/caesarCypher.test.js
+++ src/__tests__/caesarCypher.test.js
@@ -4,3 +4,7 @@ import { caesarCypher } from '../caesarCypher';
 test('Shifts all characters by one letter', () => {
   expect(caesarCypher('abc', 1)).toBe('bcd');
 });
+
+test('Shifts all characters by three letters', () => {
+  expect(caesarCypher('xyz', 3)).toBe('abc');
+});
