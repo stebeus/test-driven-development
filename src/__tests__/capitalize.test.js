0bebf3f91996291ce355dc7cf679dddcbb6acb28
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-01 14:23:55 -0300

    fix: insert toBe method after  expect function

--- src/__tests__/capitalize.test.js
+++ src/__tests__/capitalize.test.js
@@ -2,5 +2,5 @@ import { expect, test } from 'vitest';
 import { capitalize } from '../components/capitalize';
 
 test('Capitalizes the first character of the string', () => {
-  expect(capitalize('hello, world!').toBe('Hello, world!'));
+  expect(capitalize('hello, world!')).toBe('Hello, world!');
 });
