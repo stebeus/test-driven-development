433b7c74e4134b574ceff7b3ca7b8528313a501b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-01 14:55:44 -0300

    fix: use commas instead of the operators in all function calls

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -2,17 +2,17 @@ import { expect, test } from 'vitest';
 import { add, subtract, multiply, divide } from '../components/calculator';
 
 test('Sums two addends correctly', () => {
-  expect(add(1 + 2)).toBe(3);
+  expect(add(1, 2)).toBe(3);
 });
 
 test('Subtracts two operands correctly', () => {
-  expect(subtract(4 - 5)).toBe(-1);
+  expect(subtract(4, 5)).toBe(-1);
 });
 
 test('Multiplies two factors', () => {
-  expect(multiply(6 * 7)).toBe(42);
+  expect(multiply(6, 7)).toBe(42);
 });
 
 test('Divides two operands', () => {
-  expect(divide(10 / 5)).toBe(2);
+  expect(divide(10, 5)).toBe(2);
 });
