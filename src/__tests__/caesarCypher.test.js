19603e67587615c1310fce2bbddbce5b95126aab
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-01 16:40:47 -0300

    feat: create one letter shift test

--- src/__tests__/caesarCypher.test.js
+++ src/__tests__/caesarCypher.test.js
@@ -1,2 +1,6 @@
 import { expect, test } from 'vitest';
 import { caesarCypher } from '../caesarCypher';
+
+test('Shifts all characters by one letter', () => {
+  expect(caesarCypher('abc', 1)).toBe('bcd');
+});
