71c14eaa75b60628e316afcb1a4eec24de0ad752
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-01 14:54:05 -0300

    feat: create multiplication operator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,4 +2,6 @@ const add = (addendOne, addendTwo) => addendOne + addendTwo;
 
 const subtract = (minuend, subtrahend) => minuend - subtrahend;
 
-export { add, subtract };
+const multiply = (multiplicand, multiplier) => multiplicand * multiplier;
+
+export { add, subtract, multiply };
