4aeb264072e95d541268e5ba1923c922fc1235f5
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-02 15:47:54 -0300

    refactor: replace magic value with alphabet length in charCode

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -1,4 +1,5 @@
 export function caesarCipher(string, key) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
   let cipher = '';
 
   for (const char in string) {
@@ -8,9 +9,9 @@ export function caesarCipher(string, key) {
     let charCode = string.charCodeAt(char) + key;
 
     if (character === character.toLowerCase()) {
-      if (charCode > 122) charCode -= 26;
+      if (charCode > 122) charCode -= alphabet.length;
     } else if (charCode > 90) {
-      charCode -= 26;
+      charCode -= alphabet.length;
     }
 
     cipher += isLetter ? String.fromCharCode(charCode) : character;
