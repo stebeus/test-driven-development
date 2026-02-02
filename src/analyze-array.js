eaa88b7e45242aba71c265c21271cefb604efdbb
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-02 13:05:29 -0300

    fix: use the spread operator in the Math methods

--- src/analyze-array.js
+++ src/analyze-array.js
@@ -3,8 +3,8 @@ import { mean } from './calculator';
 export function analyzeArray(array) {
   return {
     average: mean(array),
-    minimum: Math.min(array),
-    maximum: Math.max(array),
+    minimum: Math.min(...array),
+    maximum: Math.max(...array),
     length: array.length,
   };
 }
