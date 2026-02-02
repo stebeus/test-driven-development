6ccb01a18bd34db319bde6ca6e22185646283ad5
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-02 10:27:32 -0300

    feat: create array analyzer

--- src/analyze-array.js
+++ src/analyze-array.js
@@ -1,1 +1,10 @@
 import { mean } from './calculator';
+
+export function analyzeArray(array) {
+  return {
+    average: mean(array),
+    minimum: Math.min(array),
+    maximum: Math.max(array),
+    length: array.length,
+  };
+}
