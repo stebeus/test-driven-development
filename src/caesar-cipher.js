4b014839a2a464fe86266ceace26164727c8f47d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-02 14:42:16 -0300

    feat!: finish caesarCipher function

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -1,3 +1,20 @@
-export function caesarCipher(string, shiftFactor) {
-  return string;
+export function caesarCipher(string, key) {
+  let cipher = '';
+
+  for (const char in string) {
+    const character = string[char];
+    const isLetter = character.match(/[A-Za-z]/g);
+
+    let charCode = string.charCodeAt(char) + key;
+
+    if (character === character.toLowerCase()) {
+      if (charCode > 122) charCode -= 26;
+    } else if (charCode > 90) {
+      charCode -= 26;
+    }
+
+    cipher += isLetter ? String.fromCharCode(charCode) : character;
+  }
+
+  return cipher;
 }
