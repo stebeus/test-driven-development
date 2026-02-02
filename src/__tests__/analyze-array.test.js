1a17a11ee34e02d7bea51335a7ca8fc0ff3e6287
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-02 10:09:16 -0300

    refactor: replace object with array for better clarity

--- src/__tests__/analyze-array.test.js
+++ src/__tests__/analyze-array.test.js
@@ -1,12 +1,7 @@
 import { expect, test } from 'vitest';
 import { analyzeArray } from '../analyze-array';
 
-const object = {
-  average: 4,
-  min: 1,
-  max: 8,
-  length: 6,
-};
+const object = [1, 8, 3, 4, 2, 6];
 
 test('Returns the arithmetic mean', () => {
   expect(analyzeArray(object)).toBe(4);
