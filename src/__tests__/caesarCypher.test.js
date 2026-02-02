e974afcbb57bb8d74431727e95f4c2524535875d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-02-02 13:15:08 -0300

    feat: create three uppercase letter shift test

--- src/__tests__/caesarCypher.test.js
+++ src/__tests__/caesarCypher.test.js
@@ -9,6 +9,10 @@ test('Shifts alphabetic characters by three letters', () => {
   expect(caesarCypher('xyz', 3)).toBe('abc');
 });
 
+test('Shifts uppercase alphabetic characters by three letters', () => {
+  expect(caesarCypher('XYZ', 3)).toBe('abc');
+});
+
 test('Preserves the original capitalization and non-alphabetic characters', () => {
   expect(caesarCypher('Hello, World!', 3)).toBe('Khoor, Zruog!');
 });
